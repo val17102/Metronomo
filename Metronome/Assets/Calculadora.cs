a699f39c082505d702918dcff225228cb7343d4c
val17102 <val17102@uvg.edu.gt>
2021-08-04 21:10:42 -0600

    Agregando calidad de acordes a calculadora

--- Metronome/Assets/Calculadora.cs
+++ Metronome/Assets/Calculadora.cs
@@ -55,13 +55,47 @@ public class Calculadora : MonoBehaviour
     }
 
     void CalcularAcordesMayores(){
-        int third1 = 0, third2 = 0, c = 0;
+        int third1 = 0, third2 = 0, c1 = 0, c2 = 0;
+        int third1Q, third2Q;
+        string third1QS = "", third2QS = "";
         for (int i = 0; i < 7; i++){
             third1 = i + 2;
             third2 = i + 4;
             third1 %= 7;
             third2 %= 7;
             Debug.Log(scale[i]+","+scale[third1]+","+scale[third2]);
+            c1 = notasD[scale[i]];
+            c2 = notasD[scale[third1]];
+            if (c2 < c1){
+                c2 = c2+12;
+            }
+            third1Q = c2-c1;
+            c1 = notasD[scale[third1]];
+            c2 = notasD[scale[third2]];
+            if (c2 < c1){
+                c2 = c2+12;
+            }
+            third2Q = c2-c1;
+
+            if(third1Q == 3){
+                third1QS = "menor";
+            }else if (third1Q == 4){
+                third1QS = "mayor";
+            }
+            if(third2Q == 3){
+                third2QS = "menor";
+            }else if (third2Q == 4){
+                third2QS = "mayor";
+            }
+
+            if(third1QS == "mayor" && third2QS == "menor")
+                Debug.Log("Acorde Mayor");
+            if(third1QS == "menor" && third2QS == "mayor")
+                Debug.Log("Acorde Menor");
+            if(third1QS == "menor" && third2QS == "menor")
+                Debug.Log("Acorde Disminuido");
+            if(third1QS == "mayor" && third2QS == "mayor")
+                Debug.Log("Acorde Aumentado");
         }
     }
 }
